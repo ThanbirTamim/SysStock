52b4428a422d111dbc08868b11e311bd65e3c11a
Thanbir Tamim <sk.tamim56@gmail.com>
2025-06-25 21:18:21 +0600

    25 june 2025

--- SysStock/UI/MainPanel.cs
+++ SysStock/UI/MainPanel.cs
@@ -65,7 +65,7 @@ namespace SysStock.UI
         private void StartTimer()
         {
             timer = new Timer();
-            timer.Interval = 1000; // Update every second
+            timer.Interval = 1000; // Update every sec
             timer.Tick += (s, e) => UpdateDateTime();
             timer.Start();
         }
